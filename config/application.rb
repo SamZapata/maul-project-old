1554de86c6db89336f69ffd1c521d5a19c5b2efe
SamZapata <legionario.zapata@gmail.com>
2017-07-16 16:52:02 -0500

    corregir error en config

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 #
-config.assets.initialize_on_precompile = false
+#config.assets.initialize_on_precompile = false
 
 module TeamMaul
   class Application < Rails::Application
